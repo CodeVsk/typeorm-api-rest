8c86f0b81ecefa83809b78f44b1915d40c11e435
CodeVsk <vynycyusvieira@gmail.com>
2022-04-11 14:41:34 -0300

    [Refactor] Change DataSource values per .env

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,21 @@
+require('dotenv/config')
 import { DataSource } from "typeorm";
 import entities from '../models/index';
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    username: "postgres",
-    password: "secret",
-    database: "crud_typeorm",
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     synchronize: true,
     logging: false,
     entities: [
         entities.Product
     ],
     subscribers: [],
-    migrations: [],
+    migrations: [
+        "./migrations/*.ts"
+    ],
 })
