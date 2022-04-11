bb44273257f77c34ec6ad5f54383e00e6382726b
CodeVsk <vynycyusvieira@gmail.com>
2022-04-11 13:24:09 -0300

    [Refactor] Added :id param in route delete

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,6 +6,6 @@ const router = Router();
 
 router.get('/products', new controllers.ProductController().getAllProducts);
 router.post('/products', new controllers.ProductController().createProduct);
-router.delete('/products', new controllers.ProductController().deleteProduct);
+router.delete('/products/:id', new controllers.ProductController().deleteProduct);
 
 export default router;
