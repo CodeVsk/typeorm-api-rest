51e06c99106c0741d1bc2ecf0df52b38fa477bda
CodeVsk <vynycyusvieira@gmail.com>
2022-04-11 14:42:14 -0300

    [Update] Added update route

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get('/products', new controllers.ProductController().getAllProducts);
 router.post('/products', new controllers.ProductController().createProduct);
+router.put('/products/:id', new controllers.ProductController().updateProduct);
 router.delete('/products/:id', new controllers.ProductController().deleteProduct);
 
 export default router;
